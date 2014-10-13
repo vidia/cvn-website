e83017927092897ef63f07a5f02dc2202c34505d
David Tschida <dmtschida1@gmail.com>
2014-10-13 16:38:44 -0400

    Wrapped the update points script in a button on the admin page. Authed it.

--- site-admin.php
+++ site-admin.php
@@ -13,73 +13,89 @@ if($_SESSION["AccountType"] != "ADMIN") {
 
 <div class="container">
 
-      <div class="row">
-        <div class="col-lg-12">
-          <?php
-          // a random hello
-          $helloArray = array("Hello", "Bonjour", "Salut", "Servas", "Aloha", "Ciao", "Howdy", "Hey,", "<span rel='tooltip' style='cursor:pointer;' title='Good luck, have fun'>glhf,</span>");
-          $randHello = array_rand($helloArray);
-          ?>
-          <h1 class="page-header">Site Settings <small><?= $helloArray[$randHello] . " " . $_SESSION["Name"] . " <a href='my-account.php#breakdown' rel='tooltip' title='Total points' class='label label-info'>" . $_SESSION["Points"] . "</a>"; ?></small></h1>
-          <ol class="breadcrumb">
-            <li><a href="dashboard.php">Dashboard</a></li>
-            <li class="active">Site Settings</li>
-          </ol>
-        </div>
-      </div><!-- /.row -->
+  <div class="row">
+    <div class="col-lg-12">
+<?php
+// a random hello
+$helloArray = array("Hello", "Bonjour", "Salut", "Servas", "Aloha", "Ciao", "Howdy", "Hey,", "<span rel='tooltip' style='cursor:pointer;' title='Good luck, have fun'>glhf,</span>");
+$randHello = array_rand($helloArray);
+?>
+      <h1 class="page-header">Site Settings <small><?= $helloArray[$randHello] . " " . $_SESSION["Name"] . " <a href='my-account.php#breakdown' rel='tooltip' title='Total points' class='label label-info'>" . $_SESSION["Points"] . "</a>"; ?></small></h1>
+      <ol class="breadcrumb">
+	<li><a href="dashboard.php">Dashboard</a></li>
+	<li class="active">Site Settings</li>
+      </ol>
+    </div>
+  </div><!-- /.row -->
+
+  <div class="row">
+    <div class="col-lg-12">
+<?php
+if($_SESSION['success'] != '') {
+  echo "<div class='alert alert-success'>".$_SESSION['success']."</div>";
+  $_SESSION['success'] = '';
+}
+
+if($_SESSION['error'] != '') {
+  echo "<div class='alert alert-danger'>".$_SESSION['error']."</div>";
+  $_SESSION['error'] = '';
+}
+?>
 
       <div class="row">
-        <div class="col-lg-12">
-           <?php
-           if($_SESSION['success'] != '') {
-                    echo "<div class='alert alert-success'>".$_SESSION['success']."</div>";
-                    $_SESSION['success'] = '';
-            }
+	<div class="col-md-4">
 
-            if($_SESSION['error'] != '') {
-                    echo "<div class='alert alert-danger'>".$_SESSION['error']."</div>";
-                    $_SESSION['error'] = '';
-            }
-            ?>
+	  <div class="panel panel-default">
+	    <div class="panel-heading">
+	      <h3 class="panel-title">Manage Maintenance Mode</h3>
+	    </div>
+	    <div class="panel-body">
+    		<?php
+    		$sql = "SELECT * FROM Settings WHERE intSettingsID=1";
+    		$result = mysql_query($sql);
+    		$settings = mysql_fetch_array($result);
+    		if($settings["isMaintenance"]) :
+    		?>
+	      <a href="doSettings.php?action=disablemm" class="btn btn-default btn-block"><i class="fa fa-times"></i> Disable Maintenance Mode</a>
+	      <?php else : ?>
+	      <a href="doSettings.php?action=enablemm" class="btn btn-default btn-block"><i class="fa fa-check"></i> Enable Maintenance Mode</a>
+	      <?php endif; ?>
+	    </div>
+	  </div>
 
-            <div class="row">
-            <div class="col-md-5">
+	  <div class="panel panel-default">
+	    <div class="panel-heading">
+	      <h3 class="panel-title">Other Settings (Requires Matinance mode)</h3>
+	    </div>
+	    <div class="panel-body">
+		<?php
+		$sql = "SELECT * FROM Settings WHERE intSettingsID=1";
+		$result = mysql_query($sql);
+		$settings = mysql_fetch_array($result);
+		if($settings["isMaintenance"]) :
+		?>
+	      <a href="doPointUpdateDb.php" class="btn btn-default btn-block"><i class="fa fa-refresh"></i> Force update of all scores</a>
+	      <?php else : ?>
+	      <a class="btn btn-default btn-block"><i class="fa fa-exclamation-circle"></i> Force update of all scores</a>
+	      <?php endif; ?>
+	    </div>
+	  </div>
 
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                  <h3 class="panel-title">Manage Maintenance Mode</h3>
-                </div>
-                <div class="panel-body">
-                  <?php
-                    $sql = "SELECT * FROM Settings WHERE intSettingsID=1";
-                    $result = mysql_query($sql);
-                    $settings = mysql_fetch_array($result);
-                    if($settings["isMaintenance"]) :
-                  ?>
-                      <a href="doSettings.php?action=disablemm" class="btn btn-default btn-block"><i class="fa fa-times"></i> Disable Maintenance Mode</a>
-                  <?php else : ?>
-                      <a href="doSettings.php?action=enablemm" class="btn btn-default btn-block"><i class="fa fa-check"></i> Enable Maintenance Mode</a>
-                  <?php endif; ?>
-                </div>
-              </div>
-
-
-
-
-            </div>
-
-            <div class="col-md-7">
-              <div class="well well-sm">
-                <h3>Did You Know? <strong>Settings</strong></h3>
-                <p>Currently, the only option is to manage maintenance mode. Future features will be housed here.</p>
-                <hr />
-                <h4>Maintenance Mode</h4>
-                <p>Maintenance mode disables the ability for ushers and usher coordinators to log in. Only users designated as administrators have access to the site. Generally, this is enabled when there are site-wide changes being made.</p>
-            </div>
-            </div>
+	</div>
 
 
+	<div class="col-md-7">
+	  <div class="well well-sm">
+	    <h3>Did You Know? <strong>Settings</strong></h3>
+	    <p>Currently, the only option is to manage maintenance mode. Future features will be housed here.</p>
+	    <hr />
+	    <h4>Maintenance Mode</h4>
+	    <p>Maintenance mode disables the ability for ushers and usher coordinators to log in. Only users designated as administrators have access to the site. Generally, this is enabled when there are site-wide changes being made.</p>
+	  </div>
+	</div>
 
+
+	</div>
 
 
 
@@ -89,5 +105,5 @@ if($_SESSION["AccountType"] != "ADMIN") {
 
 
 <?php
-include_once("assets/includes/footer.php");
+  include_once("assets/includes/footer.php");
 ?>
