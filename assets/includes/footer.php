3c59425a9d583265a7acf166a42810cdb4a6094e
David Tschida <dmtschida1@gmail.com>
2014-10-24 02:18:46 -0400

    Added form to footer.

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -8,13 +8,13 @@
           <div class="col-lg-12">
             <?php
             if($_SESSION["AccountType"] == "ADMIN") {
-              $at = "| <i class='fa fa-star'></i> You are logged in as an <strong>administrator</strong>.";
+              $at = " | <i class='fa fa-star'></i> You are logged in as an <strong>administrator</strong>.";
             }
             else if($_SESSION["AccountType"] == "UC") {
-              $at = "| <i class='fa fa-star-o'></i> You are logged in as an <strong>Usher Coordinator</strong>.";
+              $at = " | <i class='fa fa-star-o'></i> You are logged in as an <strong>Usher Coordinator</strong>.";
             }
             ?>
-            <p>Copyright &copy; <a href="http://purduecvn.com">Convocations Voice Network</a> <?= date("Y") ?> <?= $at ?></p>
+            <p>Copyright &copy; <a href="http://purduecvn.com">Convocations Voice Network</a><?= date("Y") ?> | <a href="http://goo.gl/forms/tVmrUh6Sni">Feedback form</a><?= $at ?></p>
           </div>
         </div>
       </footer>
