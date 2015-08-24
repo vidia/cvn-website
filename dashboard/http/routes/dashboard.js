23b08beefc0bbf08c36d851ee907be08377500c3
David Tschida <dmtschida1@gmail.com>
2015-08-23 20:12:23 -0400

    Dashboard update, all add/remove done, sans errors and warnings for the user

--- dashboard/http/routes/dashboard.js
+++ dashboard/http/routes/dashboard.js
@@ -5,75 +5,149 @@ module.exports = function(app, imports)
     app.get("/dashboard", imports.auth.authenticate, function(req, res){
     	//TODO: Change this to a set of paallels and series functions so that the deps for variables are met. 
     	async.parallel([
-    			function(callback) {
-	    			imports.event.findAll().then(function(events) {
-	    				res.locals.events = events; 
-	    				callback(null); 
-	    			})
-    			},
-    			function(callback) {
-
-    				async.parallel([
-						function(cb) {
-							imports.attendanceType.findOrCreate({ 
-				    			where: {
-					    			name: "Requested"
-					    		},
-					    		defaults: {
-					    			points: 0, 
-					    			isEnabled: 1, 
-					    			includeShow: 1
-					    		}
-				    		}).then(function(requestType) {
-				    			requestType = requestType[0]; 
-				    			imports.logger.info("XXXXXXXXXXXXXXXXXXXXXCreated an attn Type : " + requestType); 
-				    			res.locals.requestType = requestType; 
-				    			cb(null); 
-				    		});
-						}, 
-						function(cb) {
-		    				imports.attendanceType.findOrCreate({ 
-				    			where: {
-					    			name: "Canceled"
-					    		},
-					    		defaults: {
-					    			points: 0, 
-					    			isEnabled: 1
+			function(cb) {
+				imports.attendanceType.findOrCreate({ 
+	    			where: {
+		    			name: "Requested"
+		    		},
+		    		defaults: {
+		    			points: 0, 
+		    			isEnabled: 1, 
+		    			includeShow: 1
+		    		}
+	    		}).then(function(requestType) {
+	    			requestType = requestType[0]; 
+	    			res.locals.requestType = requestType; 
+	    			cb(null); 
+	    		});
+			}, 
+			function(cb) {
+				imports.attendanceType.findOrCreate({ 
+	    			where: {
+		    			name: "Canceled"
+		    		},
+		    		defaults: {
+		    			points: 0, 
+		    			isEnabled: 1
+					}
+	    		}).then(function(cancelType, created) {
+	    			res.locals.cancelType = cancelType[0];
+	    			cb(null); 
+	    		});
+			}, 
+			function(cb) {
+				imports.attendanceType.findOrCreate({ 
+	    			where: {
+		    			name: "Confirmed"
+		    		},
+		    		defaults: {
+		    			points: 0, 
+		    			isEnabled: 1
+					}
+	    		}).then(function(confirmedType, created) {
+	    			res.locals.confirmedType = confirmedType[0];
+	    			cb(null); 
+	    		});
+			}, 
+			function(cb) {
+				imports.attendanceType.findOrCreate({ 
+	    			where: {
+		    			name: "Cut"
+		    		},
+		    		defaults: {
+		    			points: 20, 
+		    			isEnabled: 1
+					}
+	    		}).then(function(cutType, created) {
+	    			res.locals.cutType = cutType[0];
+	    			cb(null); 
+	    		});
+			}
+		], function() {
+			imports.logger.info("Created attendance types for dashboard.")
+			async.parallel([
+				function(callback){
+					imports.attendance.findAll({
+						include: [
+							{
+								model: imports.event
+							}, 
+							{
+								model: imports.attendanceType, 
+								where: {
+									uuid: res.locals.cutType.uuid
 								}
-				    		}).then(function(cancelType, created) {
-				    			res.locals.cancelType = cancelType[0];
-				    			cb(null); 
-				    		});
-		    			}
-					], function(err, results) {
-						imports.attendance.findAll({
-	    					where: {
-	    						UserUuid: req.user.uuid, 
-	    						AttendanceTypeUuid: res.locals.requestType.uuid
-	    					}
-	    				}).then(function(requested){
-	    					async.map(requested, function(attendance, cb) {
-	    						imports.event
-	    						.findOne({where: { uuid: attendance.EventUuid }})
-	    						.then(function(event) {
-	    							cb(null, event); 
-	    						})
-	    					}, function(err, result) {
-	    						res.locals.requested = result; 
-	    						callback(null);
-	    					})
-	    				});
-					});
-    			}
-    		], function(err) {
-    			async.filter(res.locals.events, function(event, callback) {
-    				imports.logger.info(event.name + " : " + res.locals.requested.indexOf(event));
-    				callback(res.locals.requested.indexOf(event) < 0); 
-    			}, function(result) {
-    				res.locals.events = result; 
-    				res.render("dashboard");    			
-    			})
-    		}
-    	); 
-    });
+							}
+						]
+					}).then(function(attendances) {
+						imports.logger.info("Got cut attendances");
+						res.locals.cutEvents = attendances; 
+						callback(null);
+					})
+				}, 
+				function(callback){
+					imports.attendance.findAll({
+						include: [
+							{
+								model: imports.event
+							}, 
+							{
+								model: imports.attendanceType, 
+								where: {
+									uuid: res.locals.requestType.uuid
+								}
+							}
+						]
+					}).then(function(events) {
+						imports.logger.info("Got requested events");
+						res.locals.requestedEvents = events; 
+						callback(null);
+					})
+				}, 
+				function(callback){
+					imports.attendance.findAll({
+						include: [
+							{
+								model: imports.event
+							}, 
+							{
+								model: imports.attendanceType, 
+								where: {
+									uuid: res.locals.confirmedType.uuid
+								}
+							}
+						]
+					}).then(function(events) {
+						imports.logger.info("Got confirmed events");
+						res.locals.confirmedEvents = events; 
+						callback(null);
+					})
+				}, 
+				function(callback){
+					imports.attendance.findAll({
+						include: [
+							{
+								model: imports.event
+							}, 
+							{
+								model: imports.attendanceType, 
+								where: {
+									uuid: {
+										$notIn: [res.locals.cutType.uuid, res.locals.confirmedType.uuid, res.locals.requestType.uuid]
+									}
+								}
+							}
+						]
+					}).then(function(events) {
+						imports.logger.info("Got available events");
+						res.locals.availableEvents = events;
+						callback(null); 
+					})
+				}
+			], function() {
+				imports.logger.info("Got events for types"); 
+				res.render("dashboard");
+			}); // end get events for types. 
+		}); //End get attendance types. 
+    }); //end .get()
 };
